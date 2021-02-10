7845b119e68aa3c50a018c2ab150aacc1e006456
Lihaozhe <55686617+Lafilosofia@users.noreply.github.com>
2021-02-10 14:02:13 +0800

    Add files via upload

---
+++ login/service/UserPwdNotMatchedException.java
@@ -0,0 +1,31 @@
+package com.lhz.lhz_Spring_SSM01.login.service;
+
+public class UserPwdNotMatchedException extends RuntimeException {
+
+	public UserPwdNotMatchedException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserPwdNotMatchedException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserPwdNotMatchedException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserPwdNotMatchedException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserPwdNotMatchedException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
