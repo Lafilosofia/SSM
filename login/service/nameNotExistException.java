b9fa55ddde96dd1827fa0fdc4b203ef66f6a22e3
Lihaozhe <55686617+Lafilosofia@users.noreply.github.com>
2021-02-10 14:01:44 +0800

    Add files via upload

---
+++ login/service/nameNotExistException.java
@@ -0,0 +1,31 @@
+package com.lhz.lhz_Spring_SSM01.login.service;
+
+public class nameNotExistException extends RuntimeException {
+
+	public nameNotExistException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public nameNotExistException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public nameNotExistException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public nameNotExistException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public nameNotExistException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
