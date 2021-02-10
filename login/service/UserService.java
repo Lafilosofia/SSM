f3651386cd1a0676d0c960edd7a6e4477371cfac
Lihaozhe <55686617+Lafilosofia@users.noreply.github.com>
2021-02-10 14:02:36 +0800

    Add files via upload

---
+++ login/service/UserService.java
@@ -0,0 +1,8 @@
+package com.lhz.lhz_Spring_SSM01.login.service;
+
+import com.lhz.lhz_Spring_SSM01.login.entity.User;
+
+public interface UserService {
+	//ÓÃ»§µÇÂ¼
+	User userLogin(String name,String pwd);
+}
